92b2473a321c81a3501d9fb92da9763b0683fbef
Professional <ustu1982@mail.ru>
2023-05-06 16:54:35 +0500

    two

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("ДЗ 6");
+        System.out.println(" ");{
+
+        }
+
+        for(int i = 0; i <= 10; i = i + 1){
+            System.out.println("Итерация цикла " + i);
+        }
+        for (int i = 10; i>=1; i = i - 1){
+            System.out.println("Интерация цикла"+i);
+        }
+
+        {
+
+        }
     }
 }
